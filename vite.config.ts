07daa0c8cde5f1e6de009719060c0909ce715f68
MR-WEBMASTERS <dealwithmeghul@gmail.com>
2025-11-07 10:01:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: "/Takshwebx/", // ✅ REPLACE with your repo name
+      base: "/MR-WEBMASTERS/" // ✅ REPLACE with your repo name
       server: {
         port: 3000,
         host: '0.0.0.0',
