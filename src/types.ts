e115973566b4187f67ada7b384affe2a5a719e44
MR-WEBMASTERS <dealwithmeghul@gmail.com>
2025-11-07 23:07:39 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,12 @@
-export type TransactionCategory = 'Cash' | 'Staff Welfare' | 'Office Expenses';
+export type TransactionCategory = 'Staff Welfare' | 'Stationary Expenses' | 'Pooja Expenses' | 'Electricity Charges';
+export type TransactionMode = 'Cash' | 'NEFT' | 'IMPS';
 
 export interface Transaction {
   id: string;
   description: string;
   amount: number;
   category: TransactionCategory | 'Deposit';
+  mode: TransactionMode | 'Deposit';
   date: string;
   type: 'debit' | 'credit';
 }
