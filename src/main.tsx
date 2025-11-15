66a49007c6f37d29b7deaa39572e98154151db34
MR-WEBMASTERS <dealwithmeghul@gmail.com>
2025-11-15 15:11:12 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// If you have global CSS (Tailwind or custom)
+import "./index.css";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+const rootElement = document.getElementById("root")!;
+const root = createRoot(rootElement);
 
-const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
