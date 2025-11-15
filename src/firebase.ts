877a5dbb3f6c8696d29a011c441d086ca275aace
MR-WEBMASTERS <dealwithmeghul@gmail.com>
2025-11-15 14:46:16 +0530

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -15,6 +15,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export auth + db
+// Export Firestore + Auth
 export const auth = getAuth(app);
 export const db = getFirestore(app);
